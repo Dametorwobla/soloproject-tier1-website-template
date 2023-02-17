621108f5bf9fa1fa0373e74bd7e418643bb98c23
David Ametorwobla <100481080+Dametorwobla@users.noreply.github.com>
2023-02-17 09:02:26 +0000

    javascript added

---
+++ main.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', function(e) {
+    const target = document.querySelector('.scroll');
+
+    var scrolled = window.pageYOffset;
+    var rate = scrolled * 0.2;
+
+    target.style.transform = "translate3d(0px, "+ rate + "px, 0px)";
+})
